2677a3929f78a50226abd73842ea3bd92d822bf7
Rikthi <rikpm8141522@gmail.com>
2025-07-07 18:26:30 +0600

    initial commit

---
+++ src/main/java/dev/rikthipranadhik/bhagkori/BhagKoriApplication.java
@@ -0,0 +1,13 @@
+package dev.rikthipranadhik.bhagkori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhagKoriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhagKoriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/rikthipranadhik/bhagkori/BhagKoriApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rikthipranadhik.bhagkori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhagKoriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
